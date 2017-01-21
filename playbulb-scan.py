573d408bb6d91010fb6c6e5f367b958f27ac5e35
Jochen Luedering <j@chaotisch.org>
2017-01-21 17:39:41 +0100

    clean up

--- playbulb-scan.py
+++ playbulb-scan.py
@@ -15,23 +15,12 @@ for service in services:
 	for characteristic in service.getCharacteristics():
 		print(characteristic)
 		print("Handle: " + str(hex(characteristic.getHandle())))
-# There seems to be something broken there
 		if characteristic.supportsRead() == True:
 			try:
-				print(characteristic.read())
-				print(list('%2x'%b for b in characteristic.read()))
+				print("    " + characteristic.read())
+				print("    " + list('%2x'%b for b in characteristic.read()))
 			except:
-				print("BLE error")
+				print("    BLE error")
 
-
-#for bite in MyPlaybulp.readCharacteristic(25):
-#	print (hex(bite))
-
-# read current color
-#print(struct.unpack("BBBB",MyPlaybulp.readCharacteristic(int(0x001b))))
-
-#sendbites = (struct.pack("BBBB",args['white'][0],args['red'][0],args['green'][0],args['blue'][0]))
-#MyPlaybulp.writeCharacteristic(int(0x001b),sendbites)
 
 MyPlaybulp.disconnect()
-#MyPlaybulp.writeCharacteristic(25,"00ff00ff03000101".encode())
