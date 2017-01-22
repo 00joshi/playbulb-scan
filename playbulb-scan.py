093a9df160b7e66533a0b5189b2cf50cd7c4d252
Jochen Luedering <j@chaotisch.org>
2017-01-22 15:27:49 +0100

    read MAC from cmd

--- playbulb-scan.py
+++ playbulb-scan.py
@@ -2,25 +2,23 @@
 from bluepy.btle import *
 import struct
 import argparse
-MyPlaybulp = Peripheral("46:92:4B:15:AC:E6")
-#parser = argparse.ArgumentParser(description='Sends color to the esp')
-#parser.add_argument('red', metavar='R',type=int,nargs=1, help='red value in decimal')
-#parser.add_argument('green', metavar='G',type=int,nargs=1, help='green value in decimal')
-#parser.add_argument('blue', metavar='B',type=int,nargs=1, help='blue value in decimal')
-#parser.add_argument('white', metavar='W',type=int,nargs=1, help='white value in decimal')
-#args = vars(parser.parse_args())
+parser = argparse.ArgumentParser(description='Scans a playbulb')
+parser.add_argument('bmac', metavar='bmac',type=str,nargs=1, help='Bluetooth Mac')
+args = vars(parser.parse_args())
+blemac = args['bmac'][0]
+MyPlaybulp = Peripheral(blemac)
 services = MyPlaybulp.getServices()
 for service in services:
 	print (service)
 	for characteristic in service.getCharacteristics():
 		print(characteristic)
-		print("Handle: " + str(hex(characteristic.getHandle())))
+		print("    Handle: " + str(hex(characteristic.getHandle())))
 		if characteristic.supportsRead() == True:
 			try:
-				print("    " + characteristic.read())
-				print("    " + list('%2x'%b for b in characteristic.read()))
+				print("        " + characteristic.read())
+				print("        " + list('%2x'%b for b in characteristic.read()))
 			except:
-				print("    BLE error")
+				print("        BLE error")
 
 
 MyPlaybulp.disconnect()
